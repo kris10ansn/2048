6dceb31b18a38c4a3dfbf3b445a0c08a9026b020
kris10ansn <kristian.nessa@protonmail.com>
2025-10-20 14:02:06 +0800

    feat: clunky key handler for all directions, in desperate need of refactor

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ const createBoard = (size: number): number[][] => {
     return new Array(size).fill(null).map(() => new Array(size).fill(0));
 };
 
+const match = <K, V>(key: K, cases: [K[], V][]): V | null => {
+    for (const [keys, value] of cases) {
+        if (keys.includes(key)) {
+            return value;
+        }
+    }
+
+    return null;
+};
+
 const main = () => {
     const canvas = document.querySelector("canvas#game") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d");
@@ -15,25 +25,61 @@ const main = () => {
     }
 
     const board = createBoard(constants.boardSize);
-    board[1][1] = 2;
-    board[1][2] = 2;
+    board[2][1] = 1;
+    board[2][2] = 2;
+    board[1][2] = 3;
 
     window.addEventListener("keydown", (event) => {
-        const left = constants.keyMap.left.includes(event.key.toLowerCase());
-        const right = constants.keyMap.right.includes(event.key.toLowerCase());
-        const up = constants.keyMap.up.includes(event.key.toLowerCase());
-        const down = constants.keyMap.down.includes(event.key.toLowerCase());
+        const direction = match(event.key.toLowerCase(), [
+            [constants.keyMap.left, "left"],
+            [constants.keyMap.right, "right"],
+            [constants.keyMap.up, "up"],
+            [constants.keyMap.down, "down"],
+        ]);
 
-        if (left) {
-            for (let y = 0; y < constants.boardSize; y++) {
-                for (let x = 1; x < constants.boardSize; x++) {
-                    if (board[y][x] > 0) {
-                        for (let i = 0; i < x; i++) {
-                            if (board[y][i] == 0) {
-                                board[y][i] = board[y][x];
-                                board[y][x] = 0;
-                                break;
-                            }
+        if (direction == null) {
+            return;
+        }
+
+        for (let i = 0; i < constants.boardSize; i++) {
+            for (let j = 0; j < constants.boardSize; j++) {
+                const y = match(direction, [
+                    [["left", "right"], i],
+                    [["up"], j],
+                    [["down"], constants.boardSize - 1 - j],
+                ])!;
+
+                const x = match(direction, [
+                    [["left"], j],
+                    [["right"], constants.boardSize - 1 - j],
+                    [["up", "down"], i],
+                ])!;
+
+                if (board[y][x] > 0) {
+                    const limit = match(direction, [
+                        [["left"], x],
+                        [["right"], constants.boardSize - 1 - x],
+                        [["up"], y],
+                        [["down"], constants.boardSize - 1 - y],
+                    ])!;
+
+                    for (let k = 0; k < limit; k++) {
+                        const yy = match(direction, [
+                            [["left", "right"], y],
+                            [["up"], k],
+                            [["down"], constants.boardSize - 1 - k],
+                        ])!;
+
+                        const xx = match(direction, [
+                            [["left"], k],
+                            [["right"], constants.boardSize - 1 - k],
+                            [["up", "down"], x],
+                        ])!;
+
+                        if (board[yy][xx] == 0) {
+                            board[yy][xx] = board[y][x];
+                            board[y][x] = 0;
+                            break;
                         }
                     }
                 }
