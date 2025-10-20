359e6c7cf4df4171c7ebf1ce966ed9710c2b3190
kris10ansn <kristian.nessa@protonmail.com>
2025-10-20 16:25:30 +0800

    refactor: extract logic into functions, reduce clutter

--- src/board.ts
+++ src/board.ts
@@ -6,29 +6,37 @@ export const createBoard = (size: number): number[][] => {
     return new Array(size).fill(null).map(() => new Array(size).fill(0));
 };
 
-const reverseRange = range.bind(null, constants.boardSize - 1, 0, -1);
-const forwardRange = range.bind(null, 0, constants.boardSize - 1, 1);
+export const isHorizontal = (direction: Direction) =>
+    direction === "right" || direction === "left";
+
+const shouldReverseIterate = (direction: Direction) =>
+    direction === "right" || direction === "down";
+
+const iterate = (direction: Direction) =>
+    shouldReverseIterate(direction)
+        ? range(constants.boardSize - 1, 0, -1)
+        : range(0, constants.boardSize - 1, 1);
+
+const iterateFrom = (x: number, y: number, direction: Direction) => {
+    const reverse = shouldReverseIterate(direction);
+
+    const delta = reverse ? 1 : -1;
+    const from = (isHorizontal(direction) ? x : y) + delta;
+    const to = reverse ? constants.boardSize - 1 : 0;
+
+    return range(from, to, delta);
+};
 
 export const slide = (board: number[][], direction: Direction) => {
-    // Reverse iteration if right or down
-    const reverse = ["right", "down"].includes(direction);
-    const horizontal = ["right", "left"].includes(direction);
-    const iterator = reverse ? reverseRange : forwardRange;
-
-    for (const y of iterator()) {
-        for (const x of iterator()) {
+    for (const y of iterate(direction)) {
+        for (const x of iterate(direction)) {
             if (board[y][x] === 0) continue;
-
-            // Loop from the current x/y to the edge of the board
-            const delta = reverse ? 1 : -1;
-            const from = (horizontal ? x : y) + delta;
-            const to = reverse ? constants.boardSize - 1 : 0;
 
             let [toX, toY] = [x, y];
 
             // Find furthest 0 with no numbers inbetween
-            for (const i of range(from, to, delta)) {
-                const [xx, yy] = horizontal ? [i, y] : [x, i];
+            for (const i of iterateFrom(x, y, direction)) {
+                const [xx, yy] = isHorizontal(direction) ? [i, y] : [x, i];
 
                 if (board[yy][xx] === 0) {
                     [toX, toY] = [xx, yy];
@@ -46,21 +54,18 @@ export const slide = (board: number[][], direction: Direction) => {
     }
 };
 
+const deltas: Record<Direction, [number, number]> = {
+    left: [1, 0],
+    right: [-1, 0],
+    up: [0, 1],
+    down: [0, -1],
+};
+
 export const merge = (board: number[][], direction: Direction) => {
-    // Reverse iteration if right or down
-    const reverse = ["right", "down"].includes(direction);
-    const iterator = reverse ? reverseRange : forwardRange;
+    const [dx, dy] = deltas[direction];
 
-    // Merge
-    const [dx, dy] = {
-        left: [1, 0],
-        right: [-1, 0],
-        up: [0, 1],
-        down: [0, -1],
-    }[direction];
-
-    for (const y of iterator()) {
-        for (const x of iterator()) {
+    for (const y of iterate(direction)) {
+        for (const x of iterate(direction)) {
             if (board[y][x] === 0) continue;
             const [xx, yy] = [x + dx, y + dy];
 
