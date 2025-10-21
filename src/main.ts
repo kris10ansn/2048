5a57abc75c85619fddaeaf142a6735661244c9f6
kris10ansn <kristian.nessa@protonmail.com>
2025-10-21 12:46:23 +0800

    extract addTile logic into method

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ class Board {
     private static SIZE = 4;
     private boxes: Node[][];
 
+    private addTile(x: number, y: number, value: number) {
+        const box = this.boxes[y][x];
+
+        const tile = document.createElement("div");
+        tile.classList.add("tile");
+        tile.textContent = value.toString();
+
+        box.appendChild(tile);
+    }
+
     public constructor(private root: Node) {
         this.boxes = [];
 
@@ -24,11 +34,8 @@ class Board {
             this.root.appendChild(row);
         }
 
-        const box = this.boxes[2][2];
-        const tile = document.createElement("div");
-        tile.classList.add("tile");
-        tile.textContent = "2";
-        box.appendChild(tile);
+        this.addTile(2, 2, 2);
+        this.addTile(1, 1, 2);
     }
 }
 
