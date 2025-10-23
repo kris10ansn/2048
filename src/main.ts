2c213bbfb910c22422e48ee2b777c1b2b6c8c585
kris10ansn <kristian.nessa@protonmail.com>
2025-10-23 16:02:57 +0800

    feat: integrate Game class and match utility for keyboard controls

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import constants, { type Direction } from "./constants";
 import { HTMLBoardHandler } from "./board";
+import { Game } from "./Game";
+import { match } from "./util/match";
 import "./style.scss";
 
 const main = () => {
@@ -8,10 +11,27 @@ const main = () => {
         throw new Error("Root element not found!");
     }
 
-    const board = new HTMLBoardHandler(root, 4);
+    const board = new HTMLBoardHandler(root, constants.boardSize);
 
     board.addTile({ x: 2, y: 2 }, 2);
     board.addTile({ x: 1, y: 1 }, 2);
+
+    const game = new Game(board, constants.boardSize);
+
+    window.addEventListener("keydown", (event) => {
+        const direction = match<string, Direction>(event.key.toLowerCase(), [
+            [constants.keyMap.left, "left"],
+            [constants.keyMap.right, "right"],
+            [constants.keyMap.up, "up"],
+            [constants.keyMap.down, "down"],
+        ]);
+
+        if (direction === null) {
+            return;
+        }
+
+        game.slide(direction);
+    });
 };
 
 main();
