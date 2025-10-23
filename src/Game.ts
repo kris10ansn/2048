cb2fefb2ea64eaaac94b8bfdcbe8ad7a5af1be69
kris10ansn <kristian.nessa@protonmail.com>
2025-10-23 15:51:42 +0800

    feat: start building Game class with slide method based on slide function from previous version

---
+++ src/Game.ts
@@ -0,0 +1,45 @@
+import type { BoardHandler, Point } from "./board";
+import {
+    createBoardIterator,
+    iterateFrom,
+    isHorizontal,
+} from "./boardIterators";
+
+export type Direction = "up" | "down" | "left" | "right";
+
+export class Game {
+    public constructor(
+        private boardHandler: BoardHandler,
+        private size: number
+    ) {}
+
+    public slide(direction: Direction) {
+        const iterator = createBoardIterator(this.size, direction);
+
+        for (const { x, y } of iterator()) {
+            if (this.boardHandler.getTile({ x, y }) === null) {
+                continue;
+            }
+
+            const destination: Point = { x, y };
+
+            // Find furthest 0 with no numbers inbetween
+            for (const i of iterateFrom(x, y, direction, this.size)) {
+                const point = isHorizontal(direction)
+                    ? { x: i, y }
+                    : { x, y: i };
+
+                if (this.boardHandler.getTile(point) !== null) {
+                    break;
+                }
+
+                [destination.x, destination.y] = [point.x, point.y];
+            }
+
+            // Move to new location if open tile was found
+            if (x !== destination.x || y !== destination.y) {
+                this.boardHandler.moveTile({ x, y }, destination);
+            }
+        }
+    }
+}
---
+++ src/boardIterators.ts
@@ -0,0 +1,39 @@
+import type { Direction } from "./Game";
+import { range } from "./util/range";
+
+export const shouldReverseIterate = (direction: Direction) =>
+    direction === "right" || direction === "down";
+
+export const isHorizontal = (direction: Direction) =>
+    direction === "left" || direction === "right";
+
+export function* iterate2d(iterator: () => Generator<number>) {
+    for (const y of iterator()) {
+        for (const x of iterator()) {
+            yield { x, y };
+        }
+    }
+}
+
+export const createBoardIterator = (size: number, direction: Direction) => {
+    const iterator = shouldReverseIterate(direction)
+        ? range.bind(null, size - 1, 0, -1)
+        : range.bind(null, 0, size - 1, 1);
+
+    return iterate2d.bind(null, iterator);
+};
+
+export const iterateFrom = (
+    x: number,
+    y: number,
+    direction: Direction,
+    size: number
+) => {
+    const reverse = shouldReverseIterate(direction);
+
+    const delta = reverse ? 1 : -1;
+    const from = (isHorizontal(direction) ? x : y) + delta;
+    const to = reverse ? size - 1 : 0;
+
+    return range(from, to, delta);
+};
