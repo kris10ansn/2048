7dfd28bd2fc683fa48b1d17bedaaf24a43679b07
kris10ansn <kristian.nessa@protonmail.com>
2025-10-24 20:31:28 +0800

    update demo: remove merge test and update initial tiles

--- src/dev/demo.ts
+++ src/dev/demo.ts
@@ -2,10 +2,6 @@ import type { BoardHandler } from "../board";
 
 export const runDemo = (board: BoardHandler) => {
     board.addTile({ x: 2, y: 2 }, 2);
-    board.addTile({ x: 2, y: 1 }, 5);
+    board.addTile({ x: 2, y: 1 }, 4);
     board.addTile({ x: 1, y: 1 }, 2);
-
-    setTimeout(() => {
-        board.mergeTile({ x: 1, y: 1 }, { x: 2, y: 1 });
-    }, 2000);
 };
