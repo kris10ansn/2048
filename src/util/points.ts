ce8de32c4542b3543db43da28c3110b12728ace7
kris10ansn <kristian.nessa@protonmail.com>
2025-10-24 20:54:58 +0800

    add directionVectors constant and multPoint function to points.ts

--- src/util/points.ts
+++ src/util/points.ts
@@ -1,6 +1,19 @@
+import type { Direction } from "../types/Direction";
 import type { Point } from "../types/Point";
+
+export const directionVectors: Record<Direction, Point> = {
+    left: { x: -1, y: 0 },
+    right: { x: 1, y: 0 },
+    up: { x: 0, y: -1 },
+    down: { x: 0, y: 1 },
+};
 
 export const addPoints = (p1: Point, p2: Point): Point => ({
     x: p1.x + p2.x,
     y: p1.y + p2.y,
+});
+
+export const multPoint = (point: Point, scalar: number): Point => ({
+    x: point.x * scalar,
+    y: point.y * scalar,
 });
