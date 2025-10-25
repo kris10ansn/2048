67569cfed3d680c676383aa197a4781393caaf3b
kris10ansn <kristian.nessa@protonmail.com>
2025-10-25 17:26:55 +0800

    remove demo tile setup after setup is now in Game.start

--- src/dev/demo.ts
+++ src/dev/demo.ts
@@ -1,7 +1,3 @@
 import type { IBoardHandler } from "../board-handlers/IBoardHandler";
 
-export const runDemo = (board: IBoardHandler) => {
-    board.addTile({ x: 2, y: 2 }, 2);
-    board.addTile({ x: 2, y: 1 }, 4);
-    board.addTile({ x: 1, y: 1 }, 2);
-};
+export const runDemo = (board: IBoardHandler) => {};
