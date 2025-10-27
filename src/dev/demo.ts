2ad8e2a54bf226792a699cdf7a26e4e05eef79b1
kris10ansn <kristian.nessa@protonmail.com>
2025-10-27 10:43:23 +0800

    update unused variable name to avoid linter warnings

--- src/dev/demo.ts
+++ src/dev/demo.ts
@@ -1,6 +1,6 @@
 import type { IBoardHandler } from "@/board-handlers/IBoardHandler";
 import type { Game } from "@/Game";
 
-export const runDemo = (board: IBoardHandler, game: Game) => {
+export const runDemo = (_board: IBoardHandler, game: Game) => {
     game.setup();
 };
