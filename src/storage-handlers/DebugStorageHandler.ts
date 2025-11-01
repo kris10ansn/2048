e07bc79abd1a8cc5b775833af0479d4a4a663507
kris10ansn <kristian.nessa@protonmail.com>
2025-11-01 15:04:12 +0800

    feat(DebugStorageHandler): allow for passing of dummy data in constructor

--- src/storage-handlers/DebugStorageHandler.ts
+++ src/storage-handlers/DebugStorageHandler.ts
@@ -1,13 +1,15 @@
 import type { IStorageHandler } from "./IStorageHandler";
 
 export class DebugStorageHandler<T> implements IStorageHandler<T> {
+    constructor(private data: T) {}
+
     public async save<K extends keyof T>(key: K, data: T[K]): Promise<void> {
         console.debug("SAVE", { key, data });
     }
 
     public async load<K extends keyof T>(key: K): Promise<T[K] | null> {
         console.debug("LOAD", key);
-        return null;
+        return this.data[key];
     }
     public async remove<K extends keyof T>(key: K): Promise<void> {
         console.debug("REMOVE", key);
