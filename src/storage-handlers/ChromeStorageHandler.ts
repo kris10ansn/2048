b3812430858d8de830a7d8207195221b5cb921e0
kris10ansn <kristian.nessa@protonmail.com>
2025-11-01 12:25:36 +0800

    feat: add ChromeStorageHandler storage-handler to use chrome.storage API

---
+++ src/storage-handlers/ChromeStorageHandler.ts
@@ -0,0 +1,29 @@
+import "chrome";
+import type { IStorageHandler } from "./IStorageHandler";
+
+// https://developer.chrome.com/docs/extensions/reference/api/storage
+export type ChromeStorageAreaName = "local" | "sync" | "managed" | "session";
+
+export class ChromeStorageHandler<T> implements IStorageHandler<T> {
+    constructor(private readonly area: ChromeStorageAreaName) {}
+
+    public async save<K extends keyof T>(key: K, data: T[K]): Promise<void> {
+        // Casting to Partial<T> as TypeScript doesn't realize that they match
+        const update = { [key]: data } as unknown as Partial<T>;
+        return this.getStorageArea().set<T>(update);
+    }
+
+    public async load<K extends keyof T>(key: K): Promise<T[K] | null> {
+        return this.getStorageArea()
+            .get<T>([key])
+            .then((data) => data[key]);
+    }
+
+    public async remove<K extends keyof T>(key: K): Promise<void> {
+        return this.getStorageArea().remove<T>([key]);
+    }
+
+    private getStorageArea() {
+        return chrome.storage[this.area];
+    }
+}
