7e276cb69354230aefd64564ab197105c3695542
kris10ansn <kristian.nessa@protonmail.com>
2025-12-26 15:32:07 -1000

    feat: add generateTileImage utility function

---
+++ src/util/generateTileImage.ts
@@ -0,0 +1,32 @@
+import { getCssVariable } from "@/util/dom";
+
+export const generateTileImage = (value: number): OffscreenCanvas => {
+    const canvas = new OffscreenCanvas(512, 512);
+    const context = canvas.getContext("2d")!;
+
+    const color =
+        getCssVariable(`--color-tile-${value}`) ??
+        getCssVariable("--color-tile-max");
+
+    context.fillStyle = color;
+    context.beginPath();
+    context.roundRect(0, 0, canvas.width, canvas.height, 0.05 * 512);
+    context.fill();
+
+    context.fillStyle =
+        value <= 4
+            ? getCssVariable("--color-text-dark")
+            : getCssVariable("--color-text-light");
+
+    const fontSize = value < 1024 ? 200 : 175;
+    context.font = `bold ${fontSize}px Arial, sans-serif`;
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+
+    const textX = canvas.width / 2;
+    const textY = (canvas.height / 2) * 1.025;
+
+    context.fillText(value.toString(), textX, textY);
+
+    return canvas;
+};
