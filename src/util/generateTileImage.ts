b23cd2c025cae4338c710190b7c3ca5e9c3d1e81
kris10ansn <kristian.nessa@protonmail.com>
2025-12-29 13:56:54 -1000

    minor refactors

--- src/util/generateTileImage.ts
+++ src/util/generateTileImage.ts
@@ -1,23 +1,26 @@
 import type { Game } from "@/Game";
 import { getCssVariable } from "@/util/dom";
 
+const getTileColor = (value: number): string => {
+    return (
+        getCssVariable(`--color-tile-${value}`) ??
+        getCssVariable("--color-tile-max")
+    );
+};
+
 export const generateTileImage = (value: number, size: number): ImageData => {
+    const cssColorTextDark = getCssVariable("--color-text-dark");
+    const cssColorTextLight = getCssVariable("--color-text-light");
+
     const canvas = new OffscreenCanvas(size, size);
     const context = canvas.getContext("2d")!;
 
-    const color =
-        getCssVariable(`--color-tile-${value}`) ??
-        getCssVariable("--color-tile-max");
+    const color = getTileColor(value);
 
     context.fillStyle = color;
     context.beginPath();
     context.roundRect(0, 0, size, size, 0.15 * size);
     context.fill();
-
-    context.fillStyle =
-        value <= 4
-            ? getCssVariable("--color-text-dark")
-            : getCssVariable("--color-text-light");
 
     const fontSize = (0.8 - value.toString().length * 0.1) * size;
     context.font = `bold ${fontSize}px Arial, sans-serif`;
@@ -26,6 +29,8 @@ export const generateTileImage = (value: number, size: number): ImageData => {
 
     const textX = size / 2;
     const textY = (size / 2) * 1.05;
+
+    context.fillStyle = value <= 4 ? cssColorTextDark : cssColorTextLight;
 
     context.fillText(value.toString(), textX, textY);
 
